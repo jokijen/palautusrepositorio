dc720aec3ed2a017c939be0a3f58a989757ab2f7
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2024-11-06 20:09:45 +0200

    Week 2 task 10

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,25 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    # print("JSON-muotoinen vastaus:")
+    # print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        if player.is_finnish():
+            players.append(player)
+
+    print("Players from FIN\n")
+
+    for player in players:
+        print(player)
+
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,13 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.nationality = dict['nationality']
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
     
     def __str__(self):
-        return self.name
+        return f'{self.name}, team {self.team}, goals {self.goals}, assists {self.assists}'
+
+    def is_finnish(self):
+        return self.nationality == 'FIN'
